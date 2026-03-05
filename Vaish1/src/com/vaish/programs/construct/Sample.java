c10366f9da78c38e902d1968c99006f93bcfc4a6
vaish <vaish@vaishnavi>
2026-03-05 13:13:05 +0530

    3rd day

---
+++ Vaish1/src/com/vaish/programs/construct/Sample.java
@@ -0,0 +1,20 @@
+package com.vaish.programs.construct;
+
+public class Sample {
+
+		int stdid;
+		String stdname;
+		int smarks;
+		
+		Sample()
+		{
+			System.out.println("This is default con");
+		}
+
+		
+		
+		
+	}
+
+
+	
