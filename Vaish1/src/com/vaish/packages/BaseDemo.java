c33f41a019acd10cca7c15fd1005192926086474
vaish <vaish@vaishnavi>
2026-03-05 12:19:26 +0530

    2nd class

---
+++ Vaish1/src/com/vaish/packages/BaseDemo.java
@@ -0,0 +1,14 @@
+package com.vaish.packages;
+
+public class BaseDemo {
+
+	public static void main(String[] args) {
+		Base ob = new Base();
+		ob.setEid(101);
+		ob.setEname("Hello");
+		System.out.println(ob.getEid());
+		System.out.println(ob.getEname());
+
+	}
+
+}
