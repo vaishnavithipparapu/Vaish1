1f5b338ca0f5d6b8124bbb1e287b25d3bcf78460
vaish <vaish@vaishnavi>
2026-03-09 17:32:27 +0530

    3rd day

---
+++ Vaish1/src/com/vaish/programs/inheritance/SingleInh.java
@@ -0,0 +1,15 @@
+package com.vaish.programs.inheritance;
+
+public class SingleInh {
+
+	public static void main(String[] args) {
+		
+		
+		SingleDemo d = new SingleDemo();		
+		d.setName("Teja");		
+		d.setSalary(10000);		
+		d.setCity("Hyd");		
+		d.setCountry("India");		
+		d.show();	
+		}
+	}
