f0a318d74d88555c866f1743474223e50bcc18e9
vaish <vaish@vaishnavi>
2026-03-10 10:42:51 +0530

    3rd day

---
+++ Vaish1/src/com/vaish/programs/inheritance/HierarchicalInh.java
@@ -0,0 +1,18 @@
+package com.vaish.programs.inheritance;
+
+public class HierarchicalInh {
+
+	public static void main(String[] args) {
+		H1 h = new H1();
+		h.H1method();
+		h.show();
+		
+		H2 h2 = new H2();
+		h2.H2method();
+		h2.show();
+		
+		H3 h3 = new H3();
+		h3.H3method();
+	}
+
+}
