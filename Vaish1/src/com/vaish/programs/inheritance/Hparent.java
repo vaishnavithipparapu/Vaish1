dfd46a32b3ea077bacdb6469cd2a4023fe0b822d
vaish <vaish@vaishnavi>
2026-03-10 10:41:31 +0530

    3rd day

---
+++ Vaish1/src/com/vaish/programs/inheritance/Hparent.java
@@ -0,0 +1,10 @@
+package com.vaish.programs.inheritance;
+
+public class Hparent {
+
+	public void show() {
+		int a=10;
+		System.out.println("This is parent class and the value of a : "+a);
+	}
+	
+}
