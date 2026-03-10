2e8d6286d7ee66f91f80b1a10b183c8d65693de5
vaish <vaish@vaishnavi>
2026-03-10 10:45:21 +0530

    3rd day

---
+++ Vaish1/src/com/vaish/programs/inheritance/H1.java
@@ -0,0 +1,16 @@
+package com.vaish.programs.inheritance;
+
+public class H1 extends Hparent{
+
+	public void H1method()
+	{
+		System.out.println("This is H1 class");
+		
+	}
+	
+	/*public void show()
+	{
+		int a=20;
+	}*/
+	
+}
